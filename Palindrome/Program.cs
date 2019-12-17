db5485707b9bad0463a6232570fe39101408851d
Sirmais <atis.sirmais@gmail.com>
2019-12-17 13:27:07 +0100

    Palindrom for test

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = new Program();
+            Console.WriteLine(item.checkPalind("state"));
+            Console.WriteLine(item.checkPalind("animal"));
+            Console.WriteLine(item.checkPalind("tirrit"));
+            Console.WriteLine(item.checkPalind("lamina"));
+            Console.WriteLine(item.checkPalind("etats"));
+            Console.WriteLine(item.checkPalind("a"));
+            Console.ReadLine();
+
+        }
+
+        public bool checkPalind(string palind)
+        {
+            int length = palind.Length;
+            //split the word and comapre from bothe sides each letter
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (palind[i] != palind[length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
